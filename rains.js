711c96c684efbc4eca956afab7799382eb40ae70
pscar <pangza115@hotmail.com>
2021-06-21 21:03:26 +0700

    +

--- rains.js
+++ rains.js
@@ -1,109 +1,27 @@
 const axios = require('axios')
 const fs = require('fs');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/rtfloodbma?readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true });
 
-var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+const get_rain = async () => {
+  try {
+    //response
+    const response = axios.get('http://www.rtfloodbma.com/api/fetch_rains');
+    const request = await response;
+    const data = request.data;
 
-db.once('open', async () => {
-  console.log("Connection Successful!");
+    return data;
 
-  // define Schema
-  const RainSchema = mongoose.model('rains', new mongoose.Schema({
-    id: String,
-    rain_24h: String,
-    rainfall_datetime: String,
-    station_type: String,
-    agency: {
-      agency_name: {
-        th: String,
-        en: String,
-        jp: String,
-      },
-      agency_shortname: {
-        th: String,
-        en: String,
-        jp: String,
-      }
-    },
-    geocode: {
-      warning_zone: String,
-      area_code: String,
-      area_name: {
-        th: String
-      },
-      amphoe_name: {
-        th: String
-      },
-      tumbon_name: {
-        th: String
-      },
-      province_code: String,
-      province_name: {
-        th: String
-      }
-    },
-    station: {
-      id: String,
-      tele_station_name: {
-        th: String
-      },
-      tele_station_lat: String,
-      tele_station_long: String,
-      tele_station_oldcode: String,
-      tele_station_type: String,
-      sub_basin_id: String
-    },
-    basin: {
-      id: String,
-      basin_code: String,
-      basin_name: {
-        th: String,
-        en: String
-      }
-    },
-    _id: String
-  }));
-
-  const getDataRain = async () => {
-    try {
-      //response
-      const req = axios.get('http://api2.thaiwater.net:9200/api/v1/thaiwater30/public/rain_24h');
-      const res = await req;
-      const datas = res.data.data;
-      //filter
-      let data = datas.filter(data => data.geocode.amphoe_name.th === 'ทุ่งสง');
-      //gen _id object
-      for (let item of data) {
-        item._id = `${item.id}_${item.station.id}`;
-      }
-      // data filter + gen_id object
-      return data;
-    } catch (error) {
-      console.log(error)
-    }
+  } catch (error) {
+    console.log(error)
   }
+}
 
-  // documents array
-  let dataRain = await getDataRain()
+const writeFile = async () => {
+  let get_rains = await get_rain();
+  const random = Math.random().toString(36).substring(7);
+  const json = await JSON.stringify(get_rains, null, 2);
+  fs.writeFileSync('json_rain/' + random + '_new' + '.json', json);
+  console.log('writeFile');
+}
 
-  for (let item of dataRain) {
-    // console.log("data => ",item)
-    const res = await RainSchema.updateOne({ _id: item._id }, item, { upsert: true });
-    // console.log(res.n);
-    // console.log(res.nModified);
-    res.n;
-    res.nModified;
-  }
-
-  const writeFiles = async () => {
-    const random = Math.random().toString(36).substring(7);
-    const json = await JSON.stringify(dataRain, null, 2);
-    fs.writeFileSync('json_rain/' + random + '_new' + '.json', json);
-    console.log("writeFiles");
-  }
-  writeFiles();
-
-});
+writeFile();
--- waters.js
+++ waters.js
@@ -1,131 +1,24 @@
 const axios = require('axios')
 const fs = require('fs');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/rtfloodbma?readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true });
 
-var db = mongoose.connection;
+const get_water = async () => {
+  try {
+    //response
+    const response = axios.get('http://www.rtfloodbma.com/api/fetch_waters');
+    const request = await response;
+    const data = request.data;
+    return data;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', async () => {
-  console.log("Connection Successful!");
-
-  const WaterSchema = mongoose.model('waters', new mongoose.Schema({
-    waterlevel_data: {
-      id: String,
-      waterlevel_datetime: String,
-      waterlevel_m: String,
-      waterlevel_msl: String,
-      flow_rate: String,
-      discharge: String,
-      storage_percent: String,
-      sort_order: String,
-      station_type: String,
-      situation_level: String,
-    },
-    agency: {
-      id: String,
-      agency_name: {
-        th: String,
-        en: String,
-        jp: String,
-      },
-      agency_shortname: {
-        th: String,
-        en: String,
-        jp: String,
-      }
-    },
-    basin: {
-      id: String,
-      basin_code: String,
-      basin_name: {
-        th: String,
-        en: String,
-      }
-    },
-    station: {
-      id: String,
-      tele_station_name: {
-        th: String,
-        en: String,
-      },
-      tele_station_lat: String,
-      tele_station_long: String,
-      tele_station_oldcode: String,
-      tele_station_type: String,
-      left_bank: String,
-      right_bank: String,
-      min_bank: String,
-      ground_level: String,
-      offset: String,
-      sub_basin_id: String,
-      agency_id: String,
-      geocode_id: String,
-      is_key_station: false,
-      warning_level_m: String,
-      critical_level_m: String,
-      critical_level_msl: String
-    },
-    geocode: {
-      area_code: String,
-      area_name: {
-        th: String,
-      },
-      amphoe_code: String,
-      amphoe_name: {
-        th: String,
-      },
-      tumbon_code: String,
-      tumbon_name: {
-        th: String,
-      },
-      province_code: String,
-      province_name: {
-        th: String,
-      }
-    },
-    _id: String
-  }));
-  
-  const getDataWater = async () => {
-    try {
-      //response
-      const req = axios.get('http://api2.thaiwater.net:9200/api/v1/thaiwater30/public/waterlevel_load');
-      const res = await req;
-      const datas = res.data.waterlevel_data.data;
-
-      let data = datas.filter(data => data.station.tele_station_name.th === 'ทุ่งสง');
-
-      for (let item of data) {
-        item._id = `${item.id}_${item.station.id}`;
-      }
-
-      return data;
-
-    } catch (error) {
-      console.log(error)
-    }
+  } catch (error) {
+    console.log(error)
   }
+}
 
-  let dataWater = await getDataWater()
-
-  for (let item of dataWater) {
-    // console.log("data => ",item)
-    const res = await WaterSchema.updateOne({ _id: item._id }, item, { upsert: true });
-    // console.log(res.n);
-    // console.log(res.nModified);
-    res.n;
-    res.nModified;
-  }
-  
-  const writeFiles = async () => {
-    const random = Math.random().toString(36).substring(7);
-    const json = await JSON.stringify(dataWater, null, 2);
-    fs.writeFileSync('json_water/' + random + '_new' + '.json', json);
-
-    console.log('writeFile');
-  }
-  writeFiles();
-
-});
+const writeFile = async () => {
+  let get_waters = await get_water();
+  const random = Math.random().toString(36).substring(7);
+  const json = await JSON.stringify(get_waters, null, 2);
+  fs.writeFileSync('json_water/' + random + '_new' + '.json', json);
+  console.log('writeFile');
+}
+writeFile();
