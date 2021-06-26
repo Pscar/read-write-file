6f98cf35a0bada11f8afa89ac772ce359689c7d6
pscar <pangza115@hotmail.com>
2021-06-26 23:28:39 +0700

    +

--- index.js
+++ index.js
@@ -1,168 +1,90 @@
 const data = require('./o2s6uc.json');
-const MongoClient = require('mongodb').MongoClient;
-const mongoParams = { useNewUrlParser: true, useUnifiedTopology: true };
-const url_db = 'mongodb://127.0.0.1:27017/rtfloodbma?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
-const client = new MongoClient(url_db, mongoParams);
 const fs = require('fs');
 
 let doc = data.kml.Document;
 
-const data_center_of_map = () => {
-  for (let item of doc) {
-    for (let long of item.LookAt) {
-      for (item_long of long.longitude) {
-        for (item_lat of long.latitude) {
-          return {
-            "longitude": item_long,
-            "latitude": item_lat
-          }
-        }
-      }
-    }
-  }
+function data_center_of_map() {
+  const center_of_map = doc[0].LookAt.map(doc_item => {
+    let param_doc = [];
+    param_doc = doc_item;
+    return param_doc
+  })
+  return center_of_map
 }
 
-const data_polygon = () => {
-  for (let item of doc) {
-    for (let data of item.Folder) {
-      if (data.Placemark) {
-        for (let body of data.Placemark) {
-          if (body.MultiGeometry) {
-            for (let multigeometry of body.MultiGeometry) {
-              if (multigeometry.Polygon) {
-                for (let polygon of multigeometry.Polygon) {
-                  if (polygon.outerBoundaryIs) {
-                    for (let outerBoundaryIs of polygon.outerBoundaryIs) {
-                      if (outerBoundaryIs.LinearRing) {
-                        for (let LinearRing of outerBoundaryIs.LinearRing) {
-                          if (LinearRing.coordinates) {
-                            for (let coordinates of LinearRing.coordinates) {
-                              if (coordinates) {
-                                const body_name = () => {
-                                  let names = body.name;
-                                  let result_names = names.filter(data_name => data_name !== undefined && data_name !== '');
-                                  let result_name = result_names.map(data_name => data_name);
-                                  let res_name = result_name.toString();
-                                  return res_name;
-                                }
-                                const name = body_name();
-                                const data_coor = () => {
-                                  let str = coordinates.toString();
-                                  let datas_str = str.split("\r\n");
-                                  let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
-                                  let res_datas = result_datas.map(item => {
-                                    let temp = item.trim().split(",")
-                                    return {
-                                      "name": name,
-                                      "coordinate": [
-                                        {
-                                          "longitude": temp[0],
-                                          "latitude": temp[1],
-                                        }
-                                      ]
-                                    }
-                                  })
-                                  return res_datas
-                                }
-                                const res_coor = data_coor();
-                                return res_coor
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
+function data_polygons() {
+  const polygons = doc[0].Folder[4].Placemark.map(blue_item => {
+    let param_blue = [];
+    param_blue = blue_item;
+
+    let str = param_blue.MultiGeometry[0].Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].toString();
+    let datas_str = str.split("\r\n");
+    let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
+
+    let res_datas = result_datas.map(item => {
+      let temp = item.trim().split(",")
+      let res_poly = {
+        "name": param_blue.name[0],
+        "coordinates": [
+          {
+            "longitude": temp[0],
+            "latitude": temp[1],
           }
-        }
+        ]
       }
-    }
-  }
+      return res_poly
+    })
+    return res_datas
+  })
+  return polygons
 }
 
-const data_boundary = () => {
-  for (let item of doc) {
-    for (let data of item.Folder) {
-      if (data.Placemark) {
-        for (let body of data.Placemark) {
-          if (body.MultiGeometry) {
-            for (let multigeometry of body.MultiGeometry) {
-              if (multigeometry.Polygon) {
-                for (let polygon of multigeometry.Polygon) {
-                  if (polygon.outerBoundaryIs) {
-                    for (let outerBoundaryIs of polygon.outerBoundaryIs) {
-                      if (outerBoundaryIs.LinearRing) {
-                        for (let LinearRing of outerBoundaryIs.LinearRing) {
-                          if (LinearRing.coordinates) {
-                            for (let coordinates of LinearRing.coordinates) {
-                              if (coordinates) {
-                                let str = coordinates.toString();
-                                let datas_str = str.split("\r\n");
-                                let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
-                                let res_datas = result_datas.map(item => {
-                                  let temp = item.trim().split(",")
-                                  return {
-                                    "longitude": temp[0],
-                                    "latitude": temp[1],
-                                  };
-                                });
-                                return res_datas
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
+function data_boundary() {
+  const boundary = doc[0].Folder[6].Placemark.map(blue_item => {
+    let param_blue = [];
+    param_blue = blue_item;
+
+    let str = param_blue.MultiGeometry[0].LineString[0].coordinates[0].toString();
+    let datas_str = str.split("\r\n");
+    let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
+
+    let res_datas = result_datas.map(item => {
+      let temp = item.trim().split(",")
+      return {
+        "longitude": temp[0],
+        "latitude": temp[1],
       }
-    }
-  }
+    })
+    return res_datas
+  })
+  return boundary
 }
-const jsons = () => {
-  const myObj = {};
 
-  const a = data_center_of_map();
-  const b = data_polygon();
-  const c = data_boundary();
+function json() {
+  const data_center = data_center_of_map();
+  const data_bon = data_boundary();
+  const data_poly = data_polygons();
 
-  myObj.center_of_map = a
-  myObj.polygons = b
-  myObj.boundary = { coordinate: c }
+  const center = { latitude: data_center[0].latitude[0], longitude: data_center[0].longitude[0] }
+  const boundary = data_bon
+  const input = { rains: {}, waters: {}, dem: {} }
+  const myObj = { input, center_of_map: center, polygons: data_poly, boundary };
 
   return myObj
+
 }
+const jsons = json();
 
-const data_to_json = jsons();
-client.connect(err => {
-
-  if (err) {
-    console.log(err.message);
-    throw new Error("failed to connect");
-  }
-
-  let datab = client.db("rtfloodbma");
-  console.log("db connected");
-
-  const data_to_json = jsons();
-  try {
-    datab.collection("kml").insertMany([data_to_json], { ordered: false, });
-    console.log("insert succeeded kml");
-  } catch (err) {
-    console.log("insert failed", err.message);
-  }
-});
+// for (let item of Object.keys(jsons)) {
+//   var capital = jsons[item];
+//   console.log(item, capital);
+// }
 
 const writeFile = () => {
   let random = Math.random().toString(36).substring(7);
-  const json = JSON.stringify(data_to_json, null, 2);
+  const json = JSON.stringify(jsons, null, 2);
   fs.writeFileSync('json/' + random + "_new" + '.json', json);
 }
 writeFile();
+
+
