0e6a7fe02de5dd037f877c7bb1bc261c75f18152
pscar <pangza115@hotmail.com>
2021-06-28 16:16:07 +0700

    +

--- index.js
+++ index.js
@@ -23,41 +23,38 @@ function data_polygons() {
 
     let res_datas = result_datas.map(item => {
       let temp = item.trim().split(",")
-      let res_poly = {
-        "name": param_blue.name[0],
-        "coordinates": [
-          {
-            "longitude": temp[0],
-            "latitude": temp[1],
-          }
-        ]
-      }
-      return res_poly
-    })
-    return res_datas
-  })
-  return polygons
-}
-
-function data_boundary() {
-  const boundary = doc[0].Folder[6].Placemark.map(blue_item => {
-    let param_blue = [];
-    param_blue = blue_item;
-
-    let str = param_blue.MultiGeometry[0].LineString[0].coordinates[0].toString();
-    let datas_str = str.split("\r\n");
-    let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
-
-    let res_datas = result_datas.map(item => {
-      let temp = item.trim().split(",")
+      //return temp;
       return {
         "longitude": temp[0],
         "latitude": temp[1],
       }
     })
-    return res_datas
+    //return res_datas;
+    return {
+      "name": param_blue.name[0],
+      "coordinates": res_datas
+    }
   })
-  return boundary
+  return polygons
+}
+
+function data_boundary() {
+
+  let str = doc[0].Folder[6].Placemark[0].MultiGeometry[0].LineString[0].coordinates[0].toString();
+  let datas_str = str.split("\r\n");
+  let result_datas = datas_str.filter(data_str => data_str !== undefined && data_str !== '');
+
+  let res_datas = result_datas.map(item => {
+    let temp = item.trim().split(",")
+    return {
+      "longitude": temp[0],
+      "latitude": temp[1],
+    }
+  })
+  return {
+    "coordinates": res_datas
+  }
+
 }
 
 function json() {
@@ -65,20 +62,20 @@ function json() {
   const data_bon = data_boundary();
   const data_poly = data_polygons();
 
-  const center = { latitude: data_center[0].latitude[0], longitude: data_center[0].longitude[0] }
-  const boundary = data_bon
   const input = { rains: {}, waters: {}, dem: {} }
-  const myObj = { input, center_of_map: center, polygons: data_poly, boundary };
+  const center = { latitude: data_center[0].latitude[0], longitude: data_center[0].longitude[0] }
+
+  const myObj = {
+    input: input,
+    center_of_map: center,
+    polygons: data_poly,
+    boundary: data_bon
+  };
 
   return myObj
 
 }
 const jsons = json();
-
-// for (let item of Object.keys(jsons)) {
-//   var capital = jsons[item];
-//   console.log(item, capital);
-// }
 
 const writeFile = () => {
   let random = Math.random().toString(36).substring(7);
