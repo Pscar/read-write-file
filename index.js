a15b02233355f77b6539c22909ede35e270152e1
Supakon Katika <pangza115@hotmail.com>
2021-08-02 14:04:19 +0700

    +

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ const getWeather = async () => {
     const url = `https://weather.ckartisan.com/storage/${data}`;
     console.log("url", url);
     // random name for zip file
-    let random = Math.random().toString(36).substring(7);
+    let random =
+      Math.random().toString(36).substring(7) +
+      Math.random().toString(36).substring(7);
     // output location file .zip
     const randomNameZip = `${random}.zip`;
     const output = `kmls/${randomNameZip}`;
@@ -44,7 +46,7 @@ const getWeather = async () => {
         if (err) throw err;
         console.log("The file has been saved!");
       });
-      // // unzip file to kmls folder
+      //unzip file to kmls folder
       await unZip(output, randomNameZip, number);
 
       number += 1;
@@ -53,7 +55,8 @@ const getWeather = async () => {
         `data/${number}_${randomNameZip}/2D_Base.kml`,
         "utf8"
       );
-      await writeJson(xml,randomNameZip, number);
+      await writeXmltoJson(xml, randomNameZip, number, random);
+
     } catch (error) {
       console.log("error =>", error);
     }
@@ -72,130 +75,128 @@ const unZip = async (output, randomNameZip, number) => {
   });
 };
 
-const writeJson = async (xml, randomNameZip, number) => {
-  return new Promise((resolve, reject) => {
-    xml2js.parseString(xml, { mergeAttrs: true }, (err, result) => {
-      if (err) {
-        // throw err;
-        reject(err);
-      } else {
-        resolve(result);
-        const data_center_of_map = () => {
-          const center_of_map = result.kml.Document[0].LookAt.map(
-            (doc_item) => {
-              let param_doc = [];
-              param_doc = doc_item;
-              return param_doc;
-            }
-          );
-          return center_of_map;
-        };
-        const data_polygons = () => {
-          // console.log(result.kml.Document[0].Folder[4].Placemark);
-          const polygons = result.kml.Document[0].Folder[4].Placemark.map(
-            (blue_item) => {
-              let param_blue = [];
-              param_blue = blue_item;
+const writeXmltoJson = async (xml, random) => {
+  const writeJson = new xml2js.Parser();
+  writeJson
+    .parseStringPromise(xml)
+    .then((result) => {
+      const data_center_of_map = () => {
+        const center_of_map = result.kml.Document[0].LookAt.map((doc_item) => {
+          let param_doc = [];
+          param_doc = doc_item;
+          return param_doc;
+        });
+        return center_of_map;
+      };
+      const data_polygons = () => {
+        const polygons = result.kml.Document[0].Folder[4].Placemark.map(
+          (blue_item) => {
+            let param_blue = [];
+            param_blue = blue_item;
 
-              let str =
-                param_blue.MultiGeometry[0].Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].toString();
-              let datas_str = str.split("\r\n");
-              let result_datas = datas_str.filter(
-                (data_str) => data_str !== undefined && data_str !== ""
-              );
+            let str =
+              param_blue.MultiGeometry[0].Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0].toString();
+            let datas_str = str.split("\r\n");
+            let result_datas = datas_str.filter(
+              (data_str) => data_str !== undefined && data_str !== ""
+            );
 
-              let res_datas = result_datas.map((item) => {
-                let temp = item.trim().split(",");
-                //return temp;
-                return {
-                  longitude: temp[0],
-                  latitude: temp[1],
-                };
-              });
-              //return res_datas;
+            let res_datas = result_datas.map((item) => {
+              let temp = item.trim().split(",");
+              //return temp;
               return {
-                name: param_blue.name[0],
-                coordinates: res_datas,
+                longitude: temp[0],
+                latitude: temp[1],
               };
-            }
-          );
-          return polygons;
-        };
-        const data_boundary = () => {
-          let str =
-            result.kml.Document[0].Folder[6].Placemark[0].MultiGeometry[0].LineString[0].coordinates[0].toString();
-          let datas_str = str.split("\r\n");
-          let result_datas = datas_str.filter(
-            (data_str) => data_str !== undefined && data_str !== ""
-          );
-
-          let res_datas = result_datas.map((item) => {
-            let temp = item.trim().split(",");
+            });
+            //return res_datas;
             return {
-              longitude: temp[0],
-              latitude: temp[1],
+              name: param_blue.name[0],
+              coordinates: res_datas,
             };
-          });
+          }
+        );
+        return polygons;
+      };
+      const data_boundary = () => {
+        let str =
+          result.kml.Document[0].Folder[6].Placemark[0].MultiGeometry[0].LineString[0].coordinates[0].toString();
+        let datas_str = str.split("\r\n");
+        let result_datas = datas_str.filter(
+          (data_str) => data_str !== undefined && data_str !== ""
+        );
+
+        let res_datas = result_datas.map((item) => {
+          let temp = item.trim().split(",");
           return {
-            coordinates: res_datas,
+            longitude: temp[0],
+            latitude: temp[1],
           };
+        });
+        return {
+          coordinates: res_datas,
         };
-        const data_Json = () => {
-          const data_center = data_center_of_map();
-          const data_bon = data_boundary();
-          const data_poly = data_polygons();
+      };
 
-          const input = { rains: {}, waters: {}, dem: {} };
-          const center = {
-            latitude: data_center[0].latitude[0],
-            longitude: data_center[0].longitude[0],
-          };
+      const data_Json = () => {
+        const data_center = data_center_of_map();
+        const data_bon = data_boundary();
+        const data_poly = data_polygons();
 
-          // const myObj = [
-          //   {
-          //     input: input,
-          //     center_of_map: center,
-          //     polygons: data_poly,
-          //     boundary: data_bon,
-          //   },
-          // ];
-          const myObj = {
-            input: input,
-            center_of_map: center,
-            polygons: data_poly,
-            boundary: data_bon,
-          };
-
-          return myObj;
+        const input = { rains: {}, waters: {}, dem: {} };
+        const center = {
+          latitude: data_center[0].latitude[0],
+          longitude: data_center[0].longitude[0],
         };
-        const jsons = data_Json();
 
-        const writeFile = () => {
-          // for (const [key, value] of Object.entries(jsons)) {
-          // const json = JSON.stringify(jsons, null, 2);
-          // fs.writeFileSync("json/" + `${number}_${randomNameZip}` + ".json",json);
-          client.connect(async (err) => {
-            if (err) {
-              console.log(err.message);
-              throw new Error("failed to connect");
-            }
-            let datab = client.db("rtfloodbma");
-            console.log("db connected");
-
-            for await (const [key, value] of Object.entries(jsons)) {
-              const json = JSON.stringify(jsons, null, 2);
-              fs.writeFileSync("json/" + `${number}_${randomNameZip}` + ".json",json);
-              datab.collection("kmls").insertOne(jsons);
-              console.log("insert succeeded kmls", key, value);
-            }
-
-          });
+        const myObj = {
+          _id: random,
+          input: input,
+          center_of_map: center,
+          polygons: data_poly,
+          boundary: data_bon,
         };
-        writeFile();
-      }
-      //}
+
+        return myObj;
+      };
+
+      const jsons = data_Json();
+
+      const writeJson = () => {
+        client.connect(async (err) => {
+          if (err) {
+            console.log(err.message);
+            throw new Error("failed to connect");
+          }
+          let datab = client.db("rtfloodbma");
+          console.log("db connected");
+          datab.collection("kmls").insertOne(jsons);
+          // console.log("insert succeeded kmls", key, value);
+          // for await (const [key, value] of Object.entries(jsons)) {
+          //   const json = JSON.stringify(jsons, null, 2);
+          //   fs.writeFileSync(
+          //     "json/" + `${number}_${randomNameZip}` + ".json",
+          //     json
+          //   );
+          //   datab.collection("kmls").insertOne(jsons);
+          //   console.log("insert succeeded kmls", key, value);
+          // }
+        });
+        // for (const [key, value] of Object.entries(jsons)) {
+        //   const json = JSON.stringify(jsons, null, 2);
+        //   fs.writeFileSync(
+        //     "json/" + `${number}_${randomNameZip}` + ".json",
+        //     json
+        //   );
+        // }
+        // const json = JSON.stringify(jsons, null, 2);
+        // fs.writeFileSync("json/" + `${number}_${randomNameZip}` + ".json", json);
+      };
+      writeJson();
+    })
+    .catch((err) => {
+      console.log("err", err);
     });
-  });
 };
-console.log(typeof writeJson);
+
 getWeather();
